aa260596567f3ae5bb443d4f18cf3453cc9510f1
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-08-08 18:59:19 +0900

    Update Slack.php

--- oauth/Slack/Slack.php
+++ oauth/Slack/Slack.php
@@ -2,8 +2,8 @@
 
 /**
  * @link https://www.humhub.org/
- * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
- * @license https://www.humhub.com/licences
+ * @copyright Copyright (c) 2018 HumHub GmbH & Co. KG
+ * @license https://www.humhub.org/en/licences
  */
 
 namespace humhub\modules\user\authclient;
@@ -12,7 +12,6 @@ use Yii;
 use yii\helpers\Url;
 use yii\authclient\OAuth2;
 use yii\base\ErrorException;
-use yii\base\InvalidConfigException;
 
 class Slack extends OAuth2
 {
@@ -45,49 +44,33 @@ class Slack extends OAuth2
      */
     public $apiBaseUrl = 'https://api.slack.com';
 
-    public function init() {
-        parent::init();
-        if ($this->scope === null) {
-            $this->scope = 'identity.basic';
-        }
-        // https://api.slack.com/docs/sign-in-with-slack
-        $scopes = explode(',', $this->scope);
-        if (!in_array('identity.basic', $scopes)) {
-            throw new ErrorException("If you're just logging users in, set this to identity.basic. You can't ask for identity.email, identity.team, or identity.avatar without also asking for identity.basic.");
-        }
-    }
+    /**
+     * @inheritdoc
+     */
+    public $scope = 'identity.basic';
 
     /**
-     * 
-     * @param array $params
-     * @return type
+     * @inheritdoc
      */
-    public function buildAuthUrl(array $params = []) {
-        $params['state'] = 'login';
-        return parent::buildAuthUrl($params);
+    public $attributeNames = [
+        'id',
+        'identity.basic',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    protected function initUserAttributes()
+    {
+        return $this->api('users.identity', 'GET');
     }
 
     /**
      * @inheritdoc
-     * @return type
-     * @throws \yii\base\InvalidConfigException
      */
-    protected function initUserAttributes() {
-        $params = $this->getAccessToken()->getParams();
-        if (!$params['ok']) {
-            throw new \yii\base\InvalidConfigException("Invalid Slack Configuration");
-        }
-        return $params;
-    }
-
-    /**
-     * 
-     * @return type
-     */
-    protected function defaultReturnUrl() {
-        $url = parent::defaultReturnUrl();
-        $url = str_replace('&state=login', '', $url);
-        return $url;
+    public function applyAccessTokenToRequest($request, $accessToken)
+    {
+        $request->getHeaders()->set('Authorization', 'Bearer '. $accessToken->getToken());
     }
 
     /**
