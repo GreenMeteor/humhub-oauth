297babb192b34c0faa75143bd5cee3d35990fc77
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-17 16:10:51 -0400

    Enh: HumHub OAuth demo

---
+++ oauth/HumHub/Humhub.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2018 HumHub GmbH & Co. KG
+ * @license https://www.humhub.org/en/licences
+ */
+
+namespace humhub\modules\user\authclient;
+
+use Yii;
+use yii\authclient\OAuth2;
+
+/**
+ * Humhub Authclient
+ */
+class Humhub extends Oauth2
+{
+    /**
+     * @inheritdoc
+     */
+    protected function defaultViewOptions()
+    {
+        return [
+            'popupWidth' => 860,
+            'popupHeight' => 480,
+            'cssIcon' => 'fa fa-sign-in',
+            'buttonBackgroundColor' => '#395697',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public $authUrl = 'TBA';
+
+    /**
+     * @inheritdoc
+     */
+    public $tokenUrl = 'TBA';
+
+    /**
+     * @inheritdoc
+     */
+    public $apiBaseUrl = 'TBA';
+
+    /**
+     * @inheritdoc
+     */
+    protected function initUserAttributes()
+    {
+        return $this->api('TBA', 'GET');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultName() {
+        return 'humhub';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultTitle() {
+        return 'HumHub';
+    }
+}
