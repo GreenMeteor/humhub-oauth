cac843c41b663d41353e8d9a84ff5de5d4dac2b1
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-17 16:25:52 -0400

    Enh: Storenvy internal release

--- oauth/Storenvy/Storenvy.php
+++ oauth/Storenvy/Storenvy.php
@@ -1,3 +1,79 @@
-TBA
+<?php
 
-https://developers.storenvy.com/authentication
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2016 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+namespace humhub\modules\user\authclient;
+
+use yii\authclient\OAuth2;
+
+class Storenvy extends OAuth2
+{
+    /**
+     * @inheritdoc
+     */
+    public $authUrl = 'https://www.storenvy.com/oauth/authorize';
+
+    /**
+     * @inheritdoc
+     */
+    public $tokenUrl = 'https://api.storenvy.com/oauth/token';
+    
+    /**
+     * @inheritdoc
+     */
+    public $apiBaseUrl = 'https://api.storenvy.com/v1';
+
+    /**
+     * @inheritdoc
+     */
+    public $scope = 'user';
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultViewOptions()
+    {
+        return [
+            'popupWidth' => 860,
+            'popupHeight' => 480,
+            'cssIcon' => 'fa fa-shopping-bag',
+            'buttonBackgroundColor' => '#e0492f',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function initUserAttributes()
+    {
+        return $this->api('/me', 'GET');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function applyAccessTokenToRequest($request, $accessToken)
+    {
+        $request->getHeaders()->set('Authorization', 'Bearer '. $accessToken->getToken());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultName()
+    {
+        return 'storenvy';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultTitle()
+    {
+        return 'Storenvy';
+    }
+}
